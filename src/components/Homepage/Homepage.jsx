41c34629908bb9e8c6aab79cb13ed99be55fab18
Geet Salame <geetsalame156@gmail.com>
2023-08-22 19:20:35 +0530

    added notes

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './homepage.css';
+import Notecard from '../Notecard/Notecard';
 
 function Homepage() {
+
+
+  const openNote = () => {
+    document.getElementById("ReadNote").style.display = "flex";
+    console.log("clicke")
+  }
+
+  const closeNote = () => {
+    document.getElementById("ReadNote").style.display = "none";
+  }
+
   return (
-    <div id='Homepage'>Homepage</div>
+    <div id='Homepage'>
+      <h1 onClick={openNote}>Notes</h1>
+      <div id="homecards">
+        <div onClick={openNote}><Notecard pinned="true" /></div>
+        <div onClick={openNote}><Notecard /></div>
+        <div onClick={openNote}><Notecard /></div>
+        <div onClick={openNote}><Notecard /></div>
+        <div onClick={openNote}><Notecard /></div>
+        <div onClick={openNote}><Notecard /></div>
+      </div>
+      <div id="ReadNote">
+        <img src="https://cdn-icons-png.flaticon.com/512/5368/5368396.png" alt="close" id='closeNoteIcon' onClick={closeNote} />
+        <Notecard />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Notecard/Notecard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './notecard.css';
+
+function Notecard(params) {
+    return (
+        <div id='Notecard'>
+            <div style={{display:"flex"}}>
+                <div>
+                    <h1 id="noteTitle">Title</h1>
+                    <p id="noteTagline">Tagline</p>
+                    {/* <p id="noteBody">Body</p> */}
+                </div>
+                <div id="notePinsec" className='NoteOptionsIcons'>
+                    {params.pinned === "true" ? <img src="https://cdn-icons-png.flaticon.com/512/2951/2951513.png" alt="pinned" className='notePin' /> : <img src="https://cdn-icons-png.flaticon.com/512/2951/2951412.png" alt="notPinned" className='notePin' />}
+                    {/* {params.pinned === "true" ? <img src="https://cdn-icons-png.flaticon.com/512/73/73169.png" alt="pinned" className='notePin' /> : <img src="https://cdn-icons-png.flaticon.com/512/1274/1274860.png" alt="notPinned" className='notePin' />} */}
+                </div>
+            </div>
+            <div id="noteOptions">
+                <img src="https://cdn-icons-png.flaticon.com/512/686/686094.png" alt="color" className='NoteOptionsIcons' />
+                <img src="https://cdn-icons-png.flaticon.com/512/2985/2985043.png" alt="edit" className='NoteOptionsIcons' />
+                <img src="https://cdn-icons-png.flaticon.com/512/7693/7693316.png" alt="archives" className='NoteOptionsIcons' />
+                <img src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" alt="delete" className='NoteOptionsIcons' />
+            </div>
+        </div>
+    )
+}
+
+export default Notecard;
